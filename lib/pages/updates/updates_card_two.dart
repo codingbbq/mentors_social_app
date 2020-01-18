f516c31dba6cf0118b8bf699aef44f60defb8089
Codding BBQ <codingbbq@gmail.com>
2020-01-18 11:01:12 +0530

    Updates page with a new type of card

--- lib/pages/updates/updates_card_two.dart
+++ lib/pages/updates/updates_card_two.dart
@@ -17,116 +17,29 @@ class UpdatesCardTwo extends StatelessWidget {
             ),
             margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 30.0, bottom: 20.0),
             child: Padding(
-              padding: const EdgeInsets.only(top: 30, bottom: 20, left: 30, right: 30),
+              padding: const EdgeInsets.only(top: 120.0, bottom: 20, left: 30, right: 30),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisSize: MainAxisSize.max,
                 children: <Widget>[
-                  RichText(
-                  text: TextSpan(
-                    children: <TextSpan>[
-                      TextSpan(text: "\"Never startup without adequate research. You will reach a point of no return and turn into a failed startup. Invest in your research, you will thank me later\" ", style: TextStyle(color: Color(0xFF343434), fontSize: 16.0, letterSpacing: 1.0)),
-                      TextSpan(text: "2 hours ago", style: TextStyle(fontSize: 13.0, color: Colors.grey, letterSpacing: 1.0))
-                    ]
+                  Text(
+                    "Who's a Product designer?",
+                    style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 24.0)
                   ),
-                ),
-                SizedBox(height: 20.0,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Container(
-                      width: 60,
-                      height: 60,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                          fit: BoxFit.cover,
-                          image: AssetImage(
-                            "assets/images/profile/guest1.jpg"
-                          )
-                        )
+                  SizedBox(height: 15.0,),
+                  Row(
+                    children: <Widget>[
+                      Text(
+                        "elitepixels.medium.com",
+                        style: TextStyle(color: Colors.grey, fontSize: 18.0),
                       ),
-                      child: OverflowBox(
-                        minWidth: 14,
-                        minHeight: 14,
-                        alignment: Alignment(1.30, 0.8),
-                        child: // Verified Profile
-                        Container(
-                          width: 20,
-                          height: 20,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.orange,
-                            border: Border.all(
-                              color: Colors.white,
-                              width: 2.0
-                            )
-                          ),
-                          child: Icon(
-                            Icons.done,
-                            size: 14.0,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Row(
-                      children: <Widget>[
-                        FlatButton(
-                          onPressed: (){},
-                          padding: const EdgeInsets.all(10.0),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15.0),
-                            side: BorderSide(color: Color(0xFFe7e7e7))
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Icon(
-                                Icons.comment,
-                                color: Colors.orange,
-                                size: 16.0,
-                              ),
-                              SizedBox(width: 8.0,),
-                              Text("561")
-                            ],
-                          ),
-                        ),
-                        SizedBox(width: 8.0,),
-                        FlatButton(
-                          onPressed: () {},
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(80.0)
-                          ),
-                          padding: const EdgeInsets.all(0.0),
-                          child: Ink(
-                            decoration: BoxDecoration(
-                                gradient: LinearGradient(
-                                  colors: [Colors.orange, Colors.deepOrange],
-                                  stops: [0,100]
-                                ),
-                                borderRadius: BorderRadius.circular(80.0)
-                              ),
-                            child: Container(
-                              constraints: const BoxConstraints(minWidth: 88.0, minHeight: 36.0), // min sizes for Material buttons
-                              child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Icon(
-                                  Icons.thumb_up,
-                                  color: Colors.white,
-                                  size: 16.0,
-                                ),
-                                SizedBox(width: 8.0,),
-                                Text("412", style: TextStyle(color: Colors.white),)
-                              ],
-                            ),
-                            ),
-                          )
-                        )
-                      ],
-                    )
-                  ],
-                )
+                      SizedBox(width: 10,),
+                      Icon(
+                        Icons.link,
+                        color: Colors.orange,
+                      )
+                    ],
+                  ),
                 ],
               ),
             ),
@@ -143,7 +56,30 @@ class UpdatesCardTwo extends StatelessWidget {
                 width: MediaQuery.of(context).size.width * 0.80,
               )
             ),
+          ),
+
+          Positioned(
+            top: 80.0,
+            right: 70.0,
+            child: Container(
+              width: 50,
+              height: 50,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.white,
+                  width: 3.0
+                ),
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage(
+                    "assets/images/profile/guest1.jpg"
+                  )
+                )
+              ),
+            ),
           )
+
         ],
       )
     );
